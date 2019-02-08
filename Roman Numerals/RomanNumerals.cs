341318f150df3384bdb354c0dee7ca3b2896ccec
Charles Kelly <kellyc7c@live.com>
2019-02-08 08:01:25 +0000

    Added code to pass previous test by checking if number to convert into roman numerals is five or greater and then combining "V" for five with the previous code used calculate how many "I"s should be added.

--- Roman Numerals/RomanNumerals.cs
+++ Roman Numerals/RomanNumerals.cs
@@ -11,14 +11,22 @@ namespace Roman_Numerals
         public static string ToRomanNumeral(int number)
         {
             if (number == 0) return "nulla";
-            else if (number == 1) return "I";
-            else if (number == 2) return "II";
-            else if (number == 3) return "III";
-            else if (number == 4) return "IV";
-            else if (number == 5) return "V";
-            else if (number == 6) return "VI";
 
-            return "";
+            string romanNumeral = "";
+
+            if (number >= 5)
+            {
+                romanNumeral += "V";
+                number -= 5;
+            }
+
+            
+            if (number == 1) romanNumeral += "I";
+            else if (number == 2) romanNumeral += "II";
+            else if (number == 3) romanNumeral += "III";
+            else if (number == 4) return "IV";
+
+            return romanNumeral;
         }
     }
 }
