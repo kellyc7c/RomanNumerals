467b65bdf52bb5d6792c3d689403347a8617403a
Charles Kelly <kellyc7c@live.com>
2019-02-10 21:54:52 +0000

    Moved duplicate code from "ToRomanNumeral" into private method "processRomanNumeral".

--- Roman Numerals/RomanNumerals.cs
+++ Roman Numerals/RomanNumerals.cs
@@ -8,91 +8,42 @@ namespace Roman_Numerals
 {
     public class RomanNumerals
     {
-        private int number;
-        private string romanNumeral;
+        private int remainingValueToConvert;
+        private string processedRomanNumeral;
 
 
         public string ToRomanNumeral(int number)
         {
             if (number == 0) return "nulla";
 
-            string romanNumeral = "";
-
-            while (number >= 1000)
-            {
-                romanNumeral += "M";
-                number -= 1000;
-            }
-
-            if (number >= 900)
-            {
-                romanNumeral += "CM";
-                number -= 900;
-            }
-
-            if (number >= 500)
-            {
-                romanNumeral += "D";
-                number -= 500;
-            }
-
-            if (number >= 400)
-            {
-                romanNumeral += "CD";
-                number -= 400;
-            }
-
-            while (number >= 100)
-            {
-                romanNumeral += "C";
-                number -= 100;
-            }
-
-            if (number >= 90)
-            {
-                romanNumeral += "XC";
-                number -= 90;
-            }
-
-            if (number >= 50)
-            {
-                romanNumeral += "L";
-                number -= 50;
-            }
-
-            if (number >= 40)
-            {
-                romanNumeral += "XL";
-                number -= 40;
-            }
-
-            while (number >= 10)
-            {
-                romanNumeral += "X";
-                number -= 10;
-            }
-
-            if (number >= 9)
-            {
-                romanNumeral += "IX";
-                number -= 9;
-            }
-
-
-
-            if (number >= 5)
-            {
-                romanNumeral += "V";
-                number -= 5;
-            }
-
+            remainingValueToConvert = number;
             
-            if (number == 1) romanNumeral += "I";
-            else if (number == 2) romanNumeral += "II";
-            else if (number == 3) romanNumeral += "III";
-            else if (number == 4) romanNumeral += "IV";
+            processedRomanNumeral = "";
 
-            return romanNumeral;
+            processRomanNumeral(1000, "M");
+            processRomanNumeral(900, "CM");
+            processRomanNumeral(500, "D");
+            processRomanNumeral(400, "CD");
+            processRomanNumeral(100, "C");
+            processRomanNumeral(90, "XC");
+            processRomanNumeral(50, "L");
+            processRomanNumeral(40, "XL");
+            processRomanNumeral(10, "X");
+            processRomanNumeral(9, "IX");
+            processRomanNumeral(5, "V");
+            processRomanNumeral(4, "IV");
+            processRomanNumeral(1, "I");
+
+            return processedRomanNumeral;
+        }
+
+        private void processRomanNumeral(int romanNumeralInDecimal, string romanNumeral)
+        {
+            while (remainingValueToConvert >= romanNumeralInDecimal)
+            {
+                processedRomanNumeral += romanNumeral;
+                remainingValueToConvert -= romanNumeralInDecimal;
+            }
         }
 
 
